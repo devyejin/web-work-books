a3f77263327e7beb5d05d3550645647afcb6bc27
yejin <yejinoh58@gmail.com>
2023-08-02 11:29:15 +0900

    댓글 등록 advice 예외 처리

--- src/main/java/org/zerock/b02/controller/ReplyController.java
+++ src/main/java/org/zerock/b02/controller/ReplyController.java
@@ -2,6 +2,7 @@ package org.zerock.b02.controller;
 
 
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
@@ -13,28 +14,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b02.dto.ReplyDTO;
+import org.zerock.b02.service.ReplyService;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor
 public class ReplyController {
 
-    @ApiOperation(value="Replies POST", notes = "POST 방식으로 댓글 등록") //swagger api 어노테이션, 문서화
-    @PostMapping(value="/", consumes = MediaType.APPLICATION_JSON_VALUE)  // consumes <- 받아들이는 데이터 타입 명시
-    public ResponseEntity<Map<String,Long>> register(@Valid @RequestBody ReplyDTO replyDTO,
-                                                     BindingResult bindingResult) throws BindException { //@RequestBody <- JSON문자열을 ReplyDTO로 변환해줌
-        log.info("replyDTO={}",replyDTO);
+    private final ReplyService replyService;
 
-        if(bindingResult.hasErrors()) {
+    @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록") //swagger api 어노테이션, 문서화
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)  // consumes <- 받아들이는 데이터 타입 명시
+    public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                      BindingResult bindingResult) throws BindException { //@RequestBody <- JSON문자열을 ReplyDTO로 변환해줌
+        log.info("replyDTO={}", replyDTO);
+
+        if (bindingResult.hasErrors()) {
+
             throw new BindException(bindingResult); //BindException 예외가 던져지면 @RestControllerAdvice 에서 처리하게됨
         }
 
-        Map<String, Long> resultMap = Map.of("rno", 111L);//Map.of() <- 수정불가능한 map 객체를 생성
+//        Map<String, Long> resultMap = Map.of("rno", 111L);//Map.of() <- 수정불가능한 map 객체를 생성
+        HashMap<String, Long> resultMap = new HashMap<>();
 
-        return ResponseEntity.ok(resultMap); //상태코드 200을 응답하면서, 안에 담긴 객체도 함께 전달
+        Long rno = replyService.register(replyDTO);
+
+        resultMap.put("rno", rno);
+
+//        return ResponseEntity.ok(resultMap); //상태코드 200을 응답하면서, 안에 담긴 객체도 함께 전달
+        return resultMap;
 
     }
 }
--- src/main/java/org/zerock/b02/controller/advice/CustomRestAdvice.java
+++ src/main/java/org/zerock/b02/controller/advice/CustomRestAdvice.java
@@ -1,6 +1,7 @@
 package org.zerock.b02.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -34,6 +35,24 @@ public class CustomRestAdvice { //@valid 과정에서 예외가 생기면 알려
         }
 
         return ResponseEntity.badRequest().body(errorMap); // 상태코드랑 에러객체 hashMap에 담아 전달
+    }
+
+    /**
+     * valid 중 클라이언트가 존재하지 않는 bno 요청이, SQLException 발생함 (dbms에서 DataIntegrityViolationException.class 넘어옴)
+     * 근데, 이게 타고타고 전해지다 서버에서 에러내니까 클라이언트한테는 서버에러 500대가 넘어감
+     * 그러면 클라이언트는 내가 입력값을 잘못넣었다는걸 인지못하고 서버가 이상하구나~하게되니까 Excpetion을 다르게 처리해줘야함
+     *
+     */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED) //417 요청 헤더의 요구값을 만족시키지 못해서 서버에서 요청 거부
+    public ResponseEntity<Map<String,String>> handlerFKException(Exception e) {
+        log.error(e);
+
+        HashMap<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", ""+System.currentTimeMillis());
+        errorMap.put("msg", "constraint fails");
+        return ResponseEntity.badRequest().body(errorMap);
     }
 
 }
