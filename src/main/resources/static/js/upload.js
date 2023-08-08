d68291d6511b735b3aea9d590932a7409535e734
yejin <yejinoh58@gmail.com>
2023-08-08 19:37:15 +0900

    첨부파일 업로드 뷰 처리, 첨부파일 업로드 실패시 실패했다는 화면 추가하고싶음

---
+++ src/main/resources/static/js/upload.js
@@ -0,0 +1,24 @@
+// 첨부파일을 서버에 업로드하는걸 비동기처리!
+
+async function uploadToServer(formObj) {
+
+    console.log("upload to server.....");
+    console.log((formObj));
+
+    const response = await axios({
+        method:'post',
+        url:'/upload',
+        data:formObj,
+        headers: {
+            'Content-Type' : 'multipart/form-data',
+        },
+    });
+
+    return response.data;
+}
+
+async function removeFileToServer(uuid, fileName) {
+    const response = await axios.delete(`/remove/${uuid}_${fileName}`);
+    return response.data;
+}
+
